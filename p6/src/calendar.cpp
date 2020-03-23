66c76f42f518f5b086fb322ceba3dfd48df8cd46
Victoria Manrique <alu0101122083@ull.edu.es>
2020-03-23 19:52:46 +0000

    p6 sin comentarios

--- p6/src/calendar.cpp
+++ p6/src/calendar.cpp
@@ -18,10 +18,8 @@ void calendar::makeCalendar(void){
   buildCalendar(0, col_);
 }
 
-
 void calendar::buildCalendar(int inf, int sup){
   int half;
-  // sup=sup-1;
   if(inf == (sup-1)){
     if(sup+1 > teams_) 
       board_[inf][0] = -1;
@@ -37,43 +35,24 @@ void calendar::buildCalendar(int inf, int sup){
     half = (inf+sup)/2;
     buildCalendar(inf, half);
     buildCalendar(half+1, sup);
-    // std::cout << "\nBC(" << inf << " " << sup <<")\n";
-    // std::cout << "primera llamada\n";
     fillBoard(inf, half, half-inf, sup-inf-1, half+1);
     fillBoard(half+1, sup, half-inf, sup-inf-1, inf);
-    // std::cout << "segunda llamada\n";
   }
 }
 
 void calendar::fillBoard(int inf, int sup, int infd, int supd, int ini){
-  // std::cout << "FB -> inf:" << inf << " sup:" << sup << " infd:" << infd;
-  // std::cout << " supd:" << supd << " ini:" << ini << "\n";
-  // std::cout << "antes\n";
-  // std::cout << "primer for, empieza en " << infd << " termina en " << supd << "\n";
-
   for(int j=infd; j<=supd; j++){
     if ((ini+j-infd+1) > teams_){
       board_[inf][j] = -1;
     } else {
       board_[inf][j] = ini + j - infd + 1;
     }
-    // std::cout << "board:" << inf << " " << j;
-    // std::cout << " = " << ini+j-infd+1 << " ("<<ini<<"+"<<j<<"-"<<infd<<"+1)\n";
   }
-  // std::cout << "segundo for(i), empieza en " << inf + 1 << " termina en " << sup << "\n";
-  // std::cout << "segundo for(j), empieza en " << infd + 1 << " termina en " << supd << "\n";
 
   for(int i=inf+1; i<=sup; i++){
-    // std::cout << "board:" << i << " " << infd;
-    // std::cout << " = " << board_[i-1][supd] << " (board_[" << i
-    //   <<"-1]["<<supd<<"])\n";
     board_[i][infd]=board_[i-1][supd];
-    
     for(int j=infd+1; j<=supd; j++){
-    //    std::cout << "board:" << i << " " << j;
-    // std::cout << " = " << board_[i-1][j-1] << " (board_["<<i<<"-1]["<<j<<"-1])\n";
       board_[i][j]=board_[i-1][j-1];
-
     }
   }
 }

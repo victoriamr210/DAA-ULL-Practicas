c33714c5e780bf1b6cc023c17f4bf082087c38bb
Victoria Manrique <alu0101122083@ull.edu.es>
2020-03-23 19:58:53 +0000

    sin comentarios

--- p6/src/calendarPT.cpp
+++ p6/src/calendarPT.cpp
@@ -19,7 +19,6 @@ void calendarPT::makeCalendar(void){
 
 void calendarPT::buildCalendar(int inf, int sup){
   int half;
-  // sup=sup-1;
   if(inf == (sup-1)){
     board_[inf][0] = sup+1; 
     board_[sup][0] = inf+1;
@@ -27,39 +26,20 @@ void calendarPT::buildCalendar(int inf, int sup){
     half = (inf+sup)/2;
     buildCalendar(inf, half);
     buildCalendar(half+1, sup);
-    // std::cout << "\nBC(" << inf << " " << sup <<")\n";
-    // std::cout << "primera llamada\n";
     fillBoard(inf, half, half-inf, sup-inf-1, half+1);
     fillBoard(half+1, sup, half-inf, sup-inf-1, inf);
-    // std::cout << "segunda llamada\n";
   }
 }
 
 void calendarPT::fillBoard(int inf, int sup, int infd, int supd, int ini){
-  // std::cout << "FB -> inf:" << inf << " sup:" << sup << " infd:" << infd;
-  // std::cout << " supd:" << supd << " ini:" << ini << "\n";
-  // std::cout << "antes\n";
-  // std::cout << "primer for, empieza en " << infd << " termina en " << supd << "\n";
-
   for(int j=infd; j<=supd; j++){
     board_[inf][j]=ini+j-infd+1;
-    // std::cout << "board:" << inf << " " << j;
-    // std::cout << " = " << ini+j-infd+1 << " ("<<ini<<"+"<<j<<"-"<<infd<<"+1)\n";
   }
-  // std::cout << "segundo for(i), empieza en " << inf + 1 << " termina en " << sup << "\n";
-  // std::cout << "segundo for(j), empieza en " << infd + 1 << " termina en " << supd << "\n";
 
   for(int i=inf+1; i<=sup; i++){
-    // std::cout << "board:" << i << " " << infd;
-    // std::cout << " = " << board_[i-1][supd] << " (board_[" << i
-    //   <<"-1]["<<supd<<"])\n";
     board_[i][infd]=board_[i-1][supd];
-    
     for(int j=infd+1; j<=supd; j++){
-    //    std::cout << "board:" << i << " " << j;
-    // std::cout << " = " << board_[i-1][j-1] << " (board_["<<i<<"-1]["<<j<<"-1])\n";
       board_[i][j]=board_[i-1][j-1];
-
     }
   }
 }
